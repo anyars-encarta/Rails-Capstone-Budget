0f9d6aa0b289c2c20af7aec2717addd1b529331c
anyars-encarta <anyarsencarta@gmail.com>
2023-12-12 10:09:08 +0000

    Implement Devise login

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+    # def index
+    #   @user = current_user
+    # end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index', to: 'home#index'
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
-  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # resources :avatars, only: [:index, :create, :update, :destroy]
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root to: "categories#index"
 end
