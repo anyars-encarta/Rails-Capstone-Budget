ab04419e32fc35522ef3d932ebb2b7a767878d55
anyars-encarta <anyarsencarta@gmail.com>
2023-12-13 12:08:05 +0000

    Implementing add new expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,33 @@
 class ExpensesController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+        @category = Category.find(params[:id])
+        @expenses = @category.expenses.order(created_at: :desc)
+    end
+
+    def new
+      @category = Category.find(params[:category_id])
+      @expense = Expense.new
+
+      rescue ActiveRecord::RecordNotFound
+      # Handle the case where the category is not found, such as redirecting to a different page or showing an error message
+      redirect_to categories_path, alert: 'Category not found'
+    end
+
+    def create
+      @category = Category.find(expense_params[:category_id])
+      @expense = @category.expenses.new(expense_params)
+        if @expense.save
+          redirect_to category_expenses_path(@category), notice: 'Expense was successfully added.'
+        else
+          render :new
+        end
+    end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :category_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,20 @@ Rails.application.routes.draw do
   get "/categories", to: "categories#index"
   get "categories/new", to: "categories#new"
   post "/categories", to: "categories#create"
+  
+  get "/categories/:id/expenses", to: "expenses#index", as: :category_expenses
+
+  get "expenses/new", to: "expenses#new"
+  post "/expenses", to: "expenses#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :categories do
+    resources :expenses, only: [:new, :create]
+  end
   # resources :avatars, only: [:index, :create, :update, :destroy]
   # Defines the root path route ("/")
   # root to: "categories#index"
