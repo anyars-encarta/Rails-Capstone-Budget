bc51ffa3f2951482391db44d4e1b16033d29e00a
anyars-encarta <anyarsencarta@gmail.com>
2023-12-14 16:28:15 +0000

    Correct js file

--- app/javascript/search-field.js
+++ app/javascript/search-field.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#searchIcon").click(function() {
-      $("#searchField").toggle(); // Toggle the visibility of the search field
+      $("#searchField").toggle();
     });
   
     $("#closeSearch").click(function() {
-      $("#searchField").hide(); // Hide the search field when the close button is clicked
+      $("#searchField").hide();
     });
 });
 
