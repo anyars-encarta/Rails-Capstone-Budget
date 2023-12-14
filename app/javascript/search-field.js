aa3224accdbe0c9d6b2bcdbe47ede930c9bbbd9e
anyars-encarta <anyarsencarta@gmail.com>
2023-12-14 15:42:25 +0000

    Add README

--- app/javascript/search-field.js
+++ app/javascript/search-field.js
@@ -7,3 +7,22 @@ $(document).ready(function() {
       $("#searchField").hide(); // Hide the search field when the close button is clicked
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  $("#searchInput").on('input', function() {
+    var searchText = $(this).val().toLowerCase();
+    $(".exp-group li").each(function() {
+      var categoryText = $(this).find('.cname').text().toLowerCase();
+      if (categoryText.includes(searchText)) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+
+  $("#closeSearch").click(function() {
+    $("#searchInput").val('');
+    $(".exp-group li").show();
+  });
+});
