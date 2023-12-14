7eabe7c5674f005080f989ae1b3672132e8b3288
anyars-encarta <anyarsencarta@gmail.com>
2023-12-14 18:54:13 +0000

    Add render-build.sh and update production database in database.yml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,3 +1,4 @@
 bundle install --without development:test
 RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production rails db:create
 RAILS_ENV=production bundle exec rake db:migrate
