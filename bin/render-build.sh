08a3718d7af19807e6ee3bc6ae32abe9d3062fdc
anyars-encarta <anyarsencarta@gmail.com>
2023-12-16 08:04:02 +0000

    Update render-build.sh and database.yml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,4 +1,11 @@
-bundle install --without development:test
-RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production rails db:create
-RAILS_ENV=production bundle exec rake db:migrate
+# bundle install --without development:test
+# RAILS_ENV=production bundle exec rake assets:precompile
+# RAILS_ENV=production rails db:create
+# RAILS_ENV=production bundle exec rake db:migrate
+
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
