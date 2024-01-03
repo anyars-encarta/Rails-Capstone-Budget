5840bacbb0f8f3a12e5ee1a78473745793a56996
Anyars Yussif <anyarsencarta@gmail.com>
2024-01-03 15:43:52 +0000

    Update expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,7 +6,7 @@ class Expense < ApplicationRecord
   validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   belongs_to :author, class_name: 'User'
-  belongs_to :category # Specify relationship
+  belongs_to :category
 
   def save_expense(name, amount, category_id, user_id)
     expense = Expense.new(
